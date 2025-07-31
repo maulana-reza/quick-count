db792f10c3f7f7d531a5e8fe90505eabe0420c62
maulana-reza <7392maulana@gmail.com>
2025-07-31 14:25:12 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
         }
         return redirect()->route('login');
 //    }
-});
+})->name('home');
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
