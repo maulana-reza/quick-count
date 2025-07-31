6112699af249965acebaacd6ad10388594d0f36f
maulana-reza <7392maulana@gmail.com>
2025-07-31 16:13:20 +0700

    fix

--- app/Livewire/Saksi/Dasbor.php
+++ app/Livewire/Saksi/Dasbor.php
@@ -6,6 +6,18 @@ use Livewire\Component;
 
 class Dasbor extends Component
 {
+    public ?array $datas = [];
+    public function mount()
+    {
+        $this->datas = [
+            [
+                'icon' => 'akar-file',
+                'label' => 'Progress Penginputan TPS anda',
+                'value' => \App\Models\Formulir::where('saksi_id', auth()->id())
+                    ->count(),
+            ],
+        ];
+    }
     public function render()
     {
         return view('livewire.saksi.dasbor');
--- resources/views/livewire/saksi/dasbor.blade.php
+++ resources/views/livewire/saksi/dasbor.blade.php
@@ -1,3 +1,22 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div class="grid grid-cols-1 gap-4">
+    <x-label class="text-2xl font-semibold">
+        Rekap Data
+    </x-label>
+    <div>
+        <div class="grid md:grid-cols-1 grid-cols-1 gap-2">
+            @foreach($datas as $data)
+                <div
+                    class="flex gap-5 bg-white p-3 shadow rounded dark:bg-dark-eval-1 border-turquoise-500 border-t-[6px]">
+                    <div>
+                        @svg($data['icon'], 'h-12 w-12 text-gray-400')
+                    </div>
+                    <div class="ml-2 flex-grow">
+                        <div class="text-sm text-gray-500">{{ $data['label'] }}</div>
+                        <div class="text-2xl font-semibold">{!! $data['value'] !!}</div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+    @livewire('admin.chart')
 </div>
