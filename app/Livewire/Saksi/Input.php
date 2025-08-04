6beabc46d1250ee89332a6aab95c5ff4c4a0683c
maulana-reza <7392maulana@gmail.com>
2025-08-05 02:54:02 +0700

    fix

--- app/Livewire/Saksi/Input.php
+++ app/Livewire/Saksi/Input.php
@@ -2,7 +2,9 @@
 
 namespace App\Livewire\Saksi;
 
+use App\Models\DataFormulir;
 use App\Models\Formulir;
+use App\Models\Paslon;
 use App\Models\Saksi;
 use Illuminate\Support\Facades\DB;
 use Laravolt\Indonesia\Models\Village;
@@ -45,6 +47,13 @@ class Input extends Component
                 'suara_tidak_sah' => $this->item['suara_tidak_sah'],
                 'status_form' => Formulir::BELUM_VALID,
             ]);
+            Paslon::all()->each(function ($paslon) use ($form) {
+                DataFormulir::create([
+                    'formulir_id' => $form->id,
+                    'paslon_id' => $paslon->id,
+                    'suara' => $this->item['paslon'][$paslon->id] ?? 0,
+                ]);
+            });
             if (isset($this->item['laporan_kejadian']) && $this->item['laporan_kejadian'] !== '') {
                 $form->laporan_kejadian = $this->item['laporan_kejadian'];
                 $form->foto_kejadian = $this->item['foto_kejadian'] ? $this->item['foto_kejadian']->store('saksi', 'public') : null;
@@ -52,7 +61,8 @@ class Input extends Component
                 $form->save();
             }
             $this->dispatch('show', 'Berhasil menginput data formulir')->to('livewire-toast');
-
+            DB::commit();
+            $this->item = [];
         } catch (\Throwable $e) {
             DB::rollBack();
             $this->dispatch('show', 'Gagal menginput data formulir: ' . $e->getMessage())->to('livewire-toast');
