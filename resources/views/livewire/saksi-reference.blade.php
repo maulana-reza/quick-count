eea0876c72d58ac7dcdd636711d4833d41e82e17
maulana-reza <7392maulana@gmail.com>
2025-08-04 19:19:31 +0700

    fix

--- resources/views/livewire/saksi-reference.blade.php
+++ resources/views/livewire/saksi-reference.blade.php
@@ -1,6 +1,6 @@
 <div class="custom-scrollbar dark:bg-dark-eval-1 grid grid-cols-1 gap-4">
     <div class="flex justify-between">
-        <div class="text-2xl">Saksi</div>
+        <div class="text-2xl"></div>
         <x-button type="submit" wire:click="$dispatchTo('saksi-reference-child', 'showCreateForm');"
                   class="text-blue-500">
             <x-tall-crud-icon-add/>
@@ -21,22 +21,22 @@
                wire:loading.class.delay="opacity-50">
             <thead class="bg-turquoise-500 dark:text-white text-white font-bold">
             <tr class="">
-                <td class="px-3 py-2">NIK</td>
-                <td class="px-3 py-2">Nama Lengkap</td>
-                <td class="px-3 py-2">No. TPS</td>
-                <td class="px-3 py-2">No. HP</td>
-                <td class="px-3 py-2">Foto</td>
-                <td class="px-3 py-2">Actions</td>
+                <td class="px-3 py-2 text-xs">NIK</td>
+                <td class="px-3 py-2 text-xs">Nama Lengkap</td>
+                <td class="px-3 py-2 text-xs">No. TPS</td>
+                <td class="px-3 py-2 text-xs">No. HP</td>
+                <td class="px-3 py-2 text-xs">Foto</td>
+                <td class="px-3 py-2 text-xs">Actions</td>
             </tr>
             </thead>
             <tbody class="divide-y divide-">
             @forelse($results as $result)
                 <tr class="text-left {{ $loop->even ? 'bg-gray-100 dark:bg-dark-eval-2' : 'bg-white dark:bg-dark-eval-0' }}">
-                    <td class="px-3 py-2 whitespace-pre-wrap text-sm">{{ $result->nik }}</td>
-                    <td class="px-3 py-2 whitespace-pre-wrap text-sm">{{ $result->nama }}</td>
-                    <td class="px-3 py-2 whitespace-pre-wrap text-sm">{{ $result->tps }}</td>
-                    <td class="px-3 py-2 whitespace-pre-wrap text-sm">{{ $result->no_hp }}</td>
-                    <td class="px-3 py-2 ">
+                    <td class="px-3 py-2 whitespace-pre-wrap text-xs">{{ $result->nik }}</td>
+                    <td class="px-3 py-2 whitespace-pre-wrap text-xs">{{ $result->nama }}</td>
+                    <td class="px-3 py-2 whitespace-pre-wrap text-xs">{{ $result->tps }}</td>
+                    <td class="px-3 py-2 whitespace-pre-wrap text-xs">{{ $result->no_hp }}</td>
+                    <td class="px-3 py-2 whitespace-pre-wrap text-xs">
                         @if($result->foto)
                             <img src="{{ asset('storage/' . $result->foto) }}" alt="Foto Saksi"
                                  class="w-16 h-16 object-cover rounded">
